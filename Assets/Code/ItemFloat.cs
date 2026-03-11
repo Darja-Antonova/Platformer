cbc9adfa4ca361e2af010007ed1ea9a4e6503b0b
Darja-Antonova <N00253685@iadt.ie>
2026-03-11 14:40:45 +0000

    DOTween Added

---
+++ Assets/Code/ItemFloat.cs
@@ -0,0 +1,10 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class ItemFloat : MonoBehaviour
+{
+    void Start()
+    {
+        transform.DOMoveY(transform.position.y + 0.5f, 1f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+    }
+}
