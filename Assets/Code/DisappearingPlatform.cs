ca3e2302051243e2f3a254dfc820f1ecb6acecbe
Darja-Antonova <N00253685@iadt.ie>
2026-03-11 15:45:57 +0000

    DisappearingPlatforms (Not Fully Done)

---
+++ Assets/Code/DisappearingPlatform.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class DisappearingPlatform : MonoBehaviour
+{
+    public float timeToTogglePlatform = 2;
+    public float currentTime = 0;
+    public bool enabled = true;
+
+    void Start()
+    {
+        enabled = true;
+    }
+
+    void Update()
+    {
+        currentTime = Time.deltaTime;
+        if(currentTime >= timeToTogglePlatform)
+        {
+            currentTime = 0;
+            TogglePlatform();
+        }
+    }
+
+    void TogglePlatform()
+    {
+        enabled = !enabled;
+    }
+}
--- Assets/Code/ItemFloat.cs
+++ Assets/Code/ItemFloat.cs
@@ -5,6 +5,6 @@ public class ItemFloat : MonoBehaviour
 {
     void Start()
     {
-        transform.DOMoveY(transform.position.y + 0.5f, 1f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        transform.DOMoveY(transform.position.y + 0.3f, 1f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
     }
 }
