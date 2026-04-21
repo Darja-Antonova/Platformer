4fb3402274e96f88cc392f0d4618b5cec71a0c62
Darja-Antonova <N00253685@iadt.ie>
2026-04-21 12:37:39 +0100

    Spotlight Animation (2nd not playing)

--- Assets/Code/Checkpoint.cs
+++ Assets/Code/Checkpoint.cs
@@ -5,19 +5,22 @@ public class Checkpoint : MonoBehaviour
     HealthBar checkpointPos;
     public Transform respawnPoint;
     Collider2D coll;
-    public GameObject spotlight;
+    Animator spotlight;
+    public GameObject spotlightObject;
 
     private void Awake()
     {
         checkpointPos = GameObject.FindGameObjectWithTag("Player").GetComponent<HealthBar>();
         coll = GetComponent<Collider2D>();
+        spotlight = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
-            spotlight.SetActive(true);
+            spotlightObject.SetActive(true);
+            spotlight.Play("Spotlight Animation");
             checkpointPos.UpdateCheckpoint(respawnPoint.position);
             coll.enabled = false;
         }
